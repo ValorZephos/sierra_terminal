212032c8e46b73830c1c5fa4de474c7f7c9be23b
brigadefvl <71729863+brigadefvl@users.noreply.github.com>
2022-07-29 15:54:29 +0100

    Create commands.js

---
+++ js/commands.js
@@ -0,0 +1,59 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "SIA/41!56M";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = 'mailto:forrest@fkcodes.com';
+
+about = [
+  "<br>",
+  "Sierra Industries",
+  "<br>"
+];
+
+archives = [
+  "<br>",
+  "All Sierra Industries Archives",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  "<br>"
+];
+
+admin = [
+  "<br>",
+  'All Administrative Commands',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>         What is Sierra Industries?',
+  '<span class="command">archives</span>      List of Sierra Archives',
+  '<span class="command">admin</span>         Access to Administrative Commands',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Copyright 2022 (C) Sierra Industries. All rights reserved.</span>',
+  "<br>",
+  "<br>",
+  '<span class="color2">Access Granted... Sierra Industries Terminal.</span>',
+  "<br>",
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  "<br>",
+];
